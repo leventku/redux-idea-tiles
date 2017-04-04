e5f00f0317cd9524e9e648229de1fc2c13e22ae7
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2017-04-04 21:24:04 +0100

    separated ideasItem

--- src/components/IdeasItem.js
+++ src/components/IdeasItem.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class IdeasItem extends Component {
+import { updateIdea } from '../actions';
+
+
+class IdeasItem extends Component {
+  handleFormBlur(id) {
+    this.props.updateIdea({
+      id,
+      title: this.refs.title.value,
+      body: this.refs.body.value,
+    });
+  }
+
   render() {
+    const idea = this.props.idea;
     return (
-      <div>IdeaItem</div>
+      <li className="idea-item" key={idea.id}>
+        <span className="idea-id">{idea.id}</span>
+        <span className="idea-created-date">{idea.created_date.toString()}</span>
+        <form onBlur={ this.handleFormBlur.bind(this, idea.id) }>
+          <input type="text" className="idea-title" defaultValue={idea.title} ref="title" />
+          <input type="text" className="idea-body" defaultValue={idea.body} ref="body" />
+        </form>
+      </li>
     )
   }
 }
+
+export default connect(null, { updateIdea })(IdeasItem);
--- src/components/IdeasList.js
+++ src/components/IdeasList.js
@@ -2,41 +2,28 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
-import { fetchIdeas, updateIdea } from '../actions';
+import IdeasItem from './IdeasItem';
+
+import { fetchIdeas } from '../actions';
 
 class IdeasList extends Component {
   componentWillMount () {
     this.props.fetchIdeas();
   }
 
-  handleFormBlur(id) {
-    this.props.updateIdea({
-      id,
-      title: this.refs.title.value,
-      body: this.refs.body.value,
-    });
-  }
-
   renderIdeas() {
     return this.props.ideas.map(idea => {   
       return (
-        <li className="idea-item" key={idea.id}>
-          <span className="idea-id">{idea.id}</span>
-          <span className="idea-created-date">{idea.created_date.toString()}</span>
-          <form onBlur={ this.handleFormBlur.bind(this, idea.id) }>
-            <input type="text" className="idea-title" defaultValue={idea.title} ref="title" />
-            <input type="text" className="idea-body" defaultValue={idea.body} ref="body" />
-          </form>
-        </li>
+        <IdeasItem key={idea.id} idea={idea}/>
       )
     })
   }
   
   render() {
     return (
-    <ul>
-      { this.renderIdeas() }
-    </ul>
+      <ul className="ideas-list">
+        { this.renderIdeas() }
+      </ul>
     )
   }
 }
@@ -47,4 +34,4 @@ const mapStateToProps = (appState) => {
   }
 }
 
-export default connect(mapStateToProps, { fetchIdeas, updateIdea })(IdeasList);
+export default connect(mapStateToProps, { fetchIdeas })(IdeasList);
