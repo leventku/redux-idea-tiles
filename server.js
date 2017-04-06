aca7d87a8cf4a5838192fc9e50bc30775d5c4084
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2017-04-06 09:39:38 +0100

    improved server delete and update logic to persist in memory

--- server.js
+++ server.js
@@ -16,30 +16,48 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/ideas', function(req, res) {
-  res.send([{   
+
+let ideas = [
+  {   
     "id": 1,
     "created_date": "2017-04-05T10:48:37.107Z",
     "title": "first express idea",
     "body": "body text of the first idea"
-  }]);
+  }
+]
+
+app.get('/ideas', function(req, res) {
+  res.send(ideas);
 });
 
 let nextId = 2;
 app.get('/ideas/new', function(req, res) {
-  res.send([{   
+  const newIdea = {   
     "id": nextId++,
     "created_date": new Date().toISOString(),
     "title": "",
     "body": ""
-  }]);
+  }
+  ideas.push(newIdea);
+  res.send(newIdea);
 });
 
 app.post('/idea/update', function(req, res) {
+  const updatedId = req.body.id;
+  ideas = ideas.map(idea => {
+    if (updatedId == idea.id) {
+      return Object.assign(idea, req.body);
+    }
+    return idea;
+  });
   res.json(req.body);
 });
 
 app.post('/idea/delete', function(req, res) {
+  const deletedId = req.body.id;
+  ideas = ideas.filter(({id}) => {
+    return id != deletedId;
+  });
   res.json(req.body);
 });
 
